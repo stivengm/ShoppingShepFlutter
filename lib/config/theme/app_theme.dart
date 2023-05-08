c372bb7c9b9d462444bff0c29f87de9ab90c0d10
Stiven Gomez <stivengomez@soyyo.co>
2023-05-08 12:22:02 -0500

    Ajuste de colores en tema.

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
 
+  static const primaryColor = Color(0xFF7b61ff);
 
   ThemeData getTheme() => ThemeData(
-    useMaterial3: true
+    useMaterial3: true,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: primaryColor,
+      foregroundColor: Colors.white
+    ),
   );
 
 
